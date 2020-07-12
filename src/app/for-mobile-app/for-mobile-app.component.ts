b58c9bbb10b205da9206059960139a937b7d2752
NC <chauhannabin90@gmail.com>
2020-07-12 16:37:40 +0545

    it's repsonsive now

--- src/app/for-mobile-app/for-mobile-app.component.ts
+++ src/app/for-mobile-app/for-mobile-app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ForMobileAppService} from '../for-mobile-app/for-mobile-app.service'
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-for-mobile-app',
@@ -7,12 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForMobileAppComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formobileservice:ForMobileAppService) { }
   faqs=[
     {question:'How to Install App?',answer:"this is Answer1"},
     {question:'How to make an Order?',answer:"this is Answer1"},
     {question:'How to Pay?',answer:"this is Answer1"}
   ]
   ngOnInit(): void {
+    // this.formobileservice.getFaq().subscribe(console.log);
+    this.$faq=this.formobileservice.getFaq();
+    this.$faq.subscribe(console.log);
   }
+  $faq:Observable<Object>;
 }
---
+++ src/app/for-mobile-app/for-mobile-app.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from'@angular/common/http'
+import {map} from 'rxjs/operators'
+@Injectable({
+  providedIn: 'root'
+})
+export class ForMobileAppService {
+
+  constructor(private http:HttpClient) { }
+  getFaq(){
+    const key='bf4335bac47567d5167cf53678d1f7aafad30d31';
+    const headers=new HttpHeaders({'Content-Type':"application/x-www-form-urlencoded",'Client-Service':'frontend-client','Auth-Key':key});
+    return this.http.get('https://staging.grossify.in/webapi/FaqApi/getfaqUserData/1/User',{headers:headers}).pipe(map(recived=>{
+      console.log(recived);
+      return recived;
+    }));
+  }
+}
