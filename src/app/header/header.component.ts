8473a214a0e6f4403977660b251588cbb7e95423
NC <chauhannabin90@gmail.com>
2020-07-19 23:50:44 +0545

    goinng to upload it.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,6 +10,18 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
-
+  console.log(document.getElementsByClassName('navbar-toggler'));
+  console.log(document.getElementsByClassName('navbar-nav')[0])
+  this.navslide();  
+}
+  // toggleButton= document.getElementsByClassName('navbar-toggler')[0];
+  // navbarLinks=document.getElementsByClassName('navbar-nav')[0];
+   navslide =()=>{
+    const toggleButton= document.getElementsByClassName('navbar-toggler')[0];
+    const  navbarLinks=document.getElementsByClassName('navbar-nav')[0];
+    toggleButton.addEventListener('click',()=>{
+      navbarLinks.classList.toggle('active');
+      // navbarLinks.classList.add('mobile-overlay');
+    })
+  }  
 }
